822a1fec6155459b82581caf24ed670df6d8d1bb
aka kautinkazu <81628831+chris1384@users.noreply.github.com>
2023-05-27 22:58:19 +0200

    Create getPlayerFromPartialName_sh.lua

---
+++ functions/getPlayerFromPartialName_sh.lua
@@ -0,0 +1,12 @@
+function getPlayerFromPartialName(name)
+    local name = name and name:gsub("#%x%x%x%x%x%x", ""):lower() or nil
+    if name then
+        for _, player in ipairs(getElementsByType("player")) do
+            local name_ = getPlayerName(player):gsub("#%x%x%x%x%x%x", ""):lower()
+            if name_:find(name, 1, true) then
+                return player
+            end
+        end
+    end
+    return nil
+end

53cc19b8e6a30f839a2629885cab618a5fd2edcf
aka kautinkazu <81628831+chris1384@users.noreply.github.com>
2023-05-27 22:43:37 +0200

    Create client.lua

---
+++ events/onClientChatMessageWrapped/client.lua
@@ -0,0 +1,14 @@
+addEvent("chatMessage:serverWrap", true)
+addEventHandler("chatMessage:serverWrap", root, function(text, r, g, b, colorCoded)
+	triggerEvent("onClientChatMessageWrapped", resourceRoot, text, r, g, b, colorCoded)
+end)
+
+function functionWrapper(sourceRes, funcName, aclAllowed, luaName, luaLine, ...)
+	local args = {...}
+	if args[6] ~= true then
+		triggerEvent("onClientChatMessageWrapped", resourceRoot, args[1], args[2], args[3], args[4], args[5])
+		return "skip"
+	end
+	return true
+end
+addDebugHook("preFunction", functionWrapper, {"outputChatBox"})
